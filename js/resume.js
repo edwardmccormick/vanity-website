3b63f58a7be19fabb070561ee874c8e610b0d20a
edwardmccormick <ted.mccormick@gmail.com>
2021-01-23 19:15:57 -0600

    Updating resume page and index page

---
+++ js/resume.js
@@ -0,0 +1,20 @@
+// $("h6").next().hide()
+//
+// $("h6").next().next("ul").hide()
+//
+// $("h6").click(function() {
+//     console.log($(this).next());
+//     $(this).next().slideToggle(600);
+//     $(this).next().next("ul").slideToggle(1000)
+    // $(this).next().show()
+    // $(this).next().next('ul').show()
+
+})
+
+// $("h5").parent().children().not("h5").hide()
+//
+// $("h5").click(function() {
+//     $(this).parent().children().not("h5").slideToggle(600)
+// })
+
+// $(".featurette:not(.featurette-heading)").hide()
