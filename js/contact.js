83f5b234aa8d0e9180ab8e4340963c95510915e6
Ted McCormick <ted.mccormick@gmail.com>
2023-08-15 20:28:03 -0500

    Updating cfn

--- js/contact.js
+++ js/contact.js
@@ -16,19 +16,14 @@ function toggleDisable(elements) {
 function contactSubmit(e) {
     e.preventDefault();
 
-<<<<<<< HEAD
+
     let bodyJSON = {email: emailform.value,name: nameform.value,phone: phoneform.value,source: sourceform.value, bodtext: bodytextform.value}
 
     console.log("bodyJSON looks like: \n",bodyJSON)
-=======
+
     if (emailform.value.match)
     if (nameform.value.match)
-    if (phoneform.value.match(/^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g))
-
-    let bodyJSON = {email: emailform.value,name: nameform.value,phone: phoneform.value,source: sourceform.value, bodtext: bodytextform.value}
-
-    // console.log("bodyJSON looks like: \n",bodyJSON)
->>>>>>> 30d8f12d87e99dfbeff809a579f43b1c826868ea
+    if (phoneform.value.match(/^[+]?[1]?[\s]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/g)) {console.log("Valid phone number, hooray!")}
 
     toggleDisable([emailform,nameform,phoneform,sourceform,bodytextform])
     const requestOptions = {
@@ -37,7 +32,7 @@ function contactSubmit(e) {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(bodyJSON)
     };
-    fetch('https://00ln60gbx0.execute-api.us-east-1.amazonaws.com/production/contact', requestOptions)
+    fetch('https://api.mccormickhub.com/contact', requestOptions)
     // fetch("https://request-inspector.glitch.me/", requestOptions)
         .then(async response => {
             const isJson = response.headers.get('content-type')?.includes('application/json');
@@ -47,13 +42,7 @@ function contactSubmit(e) {
             // console.log("response.statusCode: ", response.statusCode)
             // console.log("data.statusCode: ", data.statusCode)
             // check for error response
-<<<<<<< HEAD
-            if (response.statusCode === 200) {
-                window.location.href = "thankyou.html"
-            }
-            else if (!response.ok) {
-                // get error message from body or default to response status
-=======
+
             if (data.statusCode == 200) {
                 console.log("Everything's okay from the async response!")
                 window.location.href = "thankyou.html"
@@ -61,7 +50,6 @@ function contactSubmit(e) {
             else if (data.statusCode == 200) {
                 // get error message from body or default to response status
                 console.log("Uh oh, got an error, how'd that happen?")
->>>>>>> 30d8f12d87e99dfbeff809a579f43b1c826868ea
                 const error = (data && data.message) || response.status;
                 return Promise.reject(error);
             }
